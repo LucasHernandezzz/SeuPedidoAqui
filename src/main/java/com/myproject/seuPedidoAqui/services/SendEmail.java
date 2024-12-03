e45df5edc32894d1ff50b2598f140701d36be370
LucasHernandezzz <lucas.hr021@outlook.com>
2024-12-03 17:07:51 -0300

    New Class SendEmail

---
+++ src/main/java/com/myproject/seuPedidoAqui/services/SendEmail.java
@@ -0,0 +1,39 @@
+package com.myproject.seuPedidoAqui.services;
+
+import com.sendgrid.*;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class SendEmail {
+
+    @Value("${SEND_EMAIL}")
+    private String sendGridApiKey;
+
+    public void sendEmail(String toEmail, String subject, String body) throws IOException {
+        Email from = new Email("lucassswk@gmail.com");
+        Email to = new Email(toEmail);
+        Content content = new Content("text/plain", body);
+        Mail mail = new Mail(from, subject, to, content);
+
+        SendGrid sg = new SendGrid(sendGridApiKey);
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+        } catch (IOException ex) {
+            throw ex;
+        }
+    }
+
+}
