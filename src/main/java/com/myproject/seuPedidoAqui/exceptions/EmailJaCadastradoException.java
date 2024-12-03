0510a0558e68cd8f426564973e489bf7a34d851c
LucasHernandezzz <lucas.hr021@outlook.com>
2024-12-03 09:27:06 -0300

    Teste commit

--- src/main/java/com/myproject/seuPedidoAqui/exceptions/EmailJaCadastradoException.java
+++ src/main/java/com/myproject/seuPedidoAqui/exceptions/EmailJaCadastradoException.java
@@ -1,5 +1,6 @@
 package com.myproject.seuPedidoAqui.exceptions;
 
+//teste
 public class EmailJaCadastradoException extends RuntimeException {
     public EmailJaCadastradoException(String message) {
         super(message);
